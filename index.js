6b3b937e3c98e58a88380ec5e09ec8e1773e7b7f
mustafaligunerduz <152859713+mustafaligunerduz@users.noreply.github.com>
2026-04-18 12:37:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const { loadPlugins } = require("./plugin-loader");
+const { runEngine } = require("./engine");
 
+const plugins = loadPlugins();
+
+function run(code) {
+  return runEngine(code, plugins);
+}
+
+// TEST
+const result = run("console.log(a)");
+console.log(result);
+
+module.exports = { run };
