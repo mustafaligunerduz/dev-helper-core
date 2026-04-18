ecffaf3aedcf6bc763134544f5738f855c485151
mustafaligunerduz <152859713+mustafaligunerduz@users.noreply.github.com>
2026-04-18 12:39:12 +0300

    Update console-check.js
    Add console-check plugin to validate console.log usage

--- plugins/console-check.js
+++ plugins/console-check.js
@@ -1,1 +1,19 @@
+module.exports = {
+  name: "console-check",
 
+  run(code) {
+    if (code.includes("console.log(a)")) {
+      return {
+        type: "error",
+        message: "a değişkeni tanımlı değil (ReferenceError)"
+      };
+    }
+
+    if (code.includes("console.log")) {
+      return {
+        type: "info",
+        message: "console.log debug kodu bulundu"
+      };
+    }
+  }
+};
