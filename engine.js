7586ec0e8c9919198019957bc966c02e68a5689c
mustafaligunerduz <152859713+mustafaligunerduz@users.noreply.github.com>
2026-04-18 12:37:58 +0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,27 @@
+function runEngine(code, plugins) {
+  let results = [];
 
+  for (let plugin of plugins) {
+    try {
+      const res = plugin.run(code);
+      if (res) results.push({
+        plugin: plugin.name,
+        ...res
+      });
+    } catch (e) {
+      results.push({
+        plugin: plugin.name,
+        type: "error",
+        message: "Plugin error: " + e.message
+      });
+    }
+  }
+
+  if (results.length === 0) {
+    return [{ type: "ok", message: "Hata bulunamadı" }];
+  }
+
+  return results;
+}
+
+module.exports = { runEngine };
