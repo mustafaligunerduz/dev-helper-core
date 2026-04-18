439e08158652021e2227854cfed20238115726ce
mustafaligunerduz <152859713+mustafaligunerduz@users.noreply.github.com>
2026-04-18 12:38:34 +0300

    Update plugin-loader.js

--- plugin-loader.js
+++ plugin-loader.js
@@ -1,1 +1,18 @@
+const fs = require("fs");
+const path = require("path");
 
+function loadPlugins() {
+  const pluginDir = path.join(__dirname, "plugins");
+  const files = fs.readdirSync(pluginDir);
+
+  let plugins = [];
+
+  for (let file of files) {
+    const plugin = require(path.join(pluginDir, file));
+    plugins.push(plugin);
+  }
+
+  return plugins;
+}
+
+module.exports = { loadPlugins };
